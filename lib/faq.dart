325e48461c2bc503d526e998f900ada5d0b506e9
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2021-05-26 15:34:07 +0200

    Added faq cards and title to about page.

--- lib/faq.dart
+++ lib/faq.dart
@@ -18,11 +18,50 @@ class _MyFaqPageState extends State<MyFaqPage> {
       appBar: AppBar(
         title: MyHeader(),
       ),
-      body: Center(
+      body: Container(
+        alignment: Alignment.topLeft,
+        margin: const EdgeInsets.all(40.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text("FAQ View"),
+            Text(
+              "About",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Card(
+                margin: EdgeInsets.all(20),
+                child:
+                    Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  ListTile(
+                      title: Text(
+                        "Ist der Impfpass sicher?",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Text(
+                              "Ja, diese App wurde mit Professoren und Experten entwickelt, die sich extrem gut auskennen mit der Materie",
+                              textAlign: TextAlign.left),
+                        ],
+                      ))
+                ])),
+            Card(
+                margin: EdgeInsets.all(20),
+                child:
+                    Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  ListTile(
+                      title: Text(
+                        "Wie schnell sehe ich neue Impfungen?",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Text(
+                              "Innerhalb weniger Sekunden nach Scannung des QR-Codes ist die Impfung durch den Arzt übertragen.",
+                              textAlign: TextAlign.left),
+                        ],
+                      ))
+                ])),
           ],
         ),
       ),
