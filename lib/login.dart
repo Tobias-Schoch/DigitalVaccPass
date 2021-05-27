c6dce42b37f6c1f53fe4ebed40669f3cc952221c
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2021-05-27 11:09:18 +0200

    Added login input form.

--- lib/login.dart
+++ lib/login.dart
@@ -14,17 +14,111 @@ class MyLoginPage extends StatefulWidget {
 class _MyLoginPageState extends State<MyLoginPage> {
   @override
   Widget build(BuildContext context) {
+    const primeColor = const Color(0xff5D5FEF);
+    const textColor = const Color(0xff263238);
     return Scaffold(
       appBar: AppBar(
         title: MyHeader(),
         elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Login View"),
-          ],
+      body: Container(
+        alignment: Alignment.topLeft,
+        margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text("Login",
+                  style: Theme.of(context).textTheme.headline4,
+                  textAlign: TextAlign.left),
+              SizedBox(height: 25),
+              new TextFormField(
+                autofocus: true,
+                cursorColor: textColor,
+                decoration: new InputDecoration(
+                  labelText: "E-Mail",
+                  fillColor: primeColor,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(12.0),
+                    borderSide: new BorderSide(color: primeColor),
+                  ),
+                  enabledBorder: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(12.0),
+                    borderSide: new BorderSide(color: textColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: primeColor)),
+                  labelStyle: new TextStyle(color: textColor),
+                ),
+                validator: (val) {
+                  if (val.length == 0) {
+                    return "Emailfeld darf nicht leer sein.";
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 25),
+              new TextFormField(
+                cursorColor: textColor,
+                decoration: new InputDecoration(
+                  labelText: "Passwort",
+                  fillColor: primeColor,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(12.0),
+                    borderSide: new BorderSide(color: primeColor),
+                  ),
+                  enabledBorder: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(12.0),
+                    borderSide: new BorderSide(color: textColor),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: primeColor)),
+                  labelStyle: new TextStyle(color: textColor),
+                ),
+                obscureText: true,
+                validator: (val) {
+                  if (val.length <= 15) {
+                    return "Passwort ist zu kurz.";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 25),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 16),
+                  primary: textColor,
+                ),
+                onPressed: () {},
+                child: const Text('Password vergessen?'),
+              ),
+              SizedBox(height: 25),
+              ConstrainedBox(
+                constraints: BoxConstraints.tightFor(height: 60),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 16),
+                    primary: primeColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12), // <-- Radius
+                    ),
+
+                  ),
+                  onPressed: () {},
+                  label: Text('Einloggen', style: new TextStyle(fontSize: 20)),
+                  icon: Icon(Icons.login),
+                ),
+              ),
+
+
+            ],
+          ),
         ),
       ),
       drawer: MyDrawer(),
