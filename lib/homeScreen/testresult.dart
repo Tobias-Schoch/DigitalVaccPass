7b265a6c706e9f73fa9e48767c070f7b512da56f
Involute1 <L_noth@yahoo.de>
2021-05-27 16:39:23 +0200

    working on testsview

--- lib/homeScreen/testresult.dart
+++ lib/homeScreen/testresult.dart
@@ -1,6 +1,7 @@
 import 'package:digital_vac_pass/util.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../drawer.dart';
 
 class MyTestPage extends StatefulWidget {
@@ -22,125 +23,79 @@ class _MyTestPageState extends State<MyTestPage> {
       body: Container(
         alignment: Alignment.topLeft,
         margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Covid-19 Test",
-                  style: Theme.of(context).textTheme.headline4,
-                  textAlign: TextAlign.left),
-              SizedBox(height: 25),
-              Card(
-                  color: pending,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  elevation: 10,
-                  child:
-                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                    ListTile(
-                      title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 18),
-                            Text("Antigen Test",
-                                style: Theme.of(context).textTheme.headline5),
-                          ]),
-                      subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 10),
-                            Text("Test-ID: 123845234",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Datum: 27.05.2021",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Arzt: Dr. Peter Schmidt",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Ausstehend",
-                                style: Theme.of(context).textTheme.bodyText1),
-                            SizedBox(height: 18),
-                          ]),
-                    ),
-                  ])),
-              SizedBox(height: 20),
-              Card(
-                  color: negative,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  elevation: 10,
-                  child:
-                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                    ListTile(
-                      title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 18),
-                            Text("Antigen Test",
-                                style: Theme.of(context).textTheme.headline5),
-                          ]),
-                      subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 10),
-                            Text("Test-ID: 123845234",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Datum: 08.05.2021",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Arzt: Dr. Peter Schmidt",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Negativ",
-                                style: Theme.of(context).textTheme.bodyText1),
-                            SizedBox(height: 18),
-                          ]),
-                    ),
-                  ])),
-              SizedBox(height: 20),
-              Card(
-                  color: positive,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  elevation: 10,
-                  child:
-                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                    ListTile(
-                      title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 18),
-                            Text("Antigen Test",
-                                style: Theme.of(context).textTheme.headline5),
-                          ]),
-                      subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 10),
-                            Text("Test-ID: 122354234",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Datum: 12.04.2021",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Arzt: Dr. Peter Schmidt",
-                                textAlign: TextAlign.left),
-                            SizedBox(height: 8),
-                            Text("Positiv",
-                                style: Theme.of(context).textTheme.bodyText1),
-                            SizedBox(height: 18),
-                          ]),
-                    ),
-                  ])),
-              SizedBox(height: 25),
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Tests",
+            style: Theme.of(context).textTheme.headline4,
+          textAlign: TextAlign.left),
+            SizedBox(height: 25),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: testsListDb.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            elevation: 10,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    child: Column(
+                                      children: <Widget>[
+                                        ListTile(
+                                          tileColor: testsListDb
+                                            .elementAt(index)
+                                            .testStatus == Status.Pending
+                                              ? pending
+                                              : testsListDb.elementAt(index).testStatus == Status.Good
+                                              ? negative : positive,
+                                          title: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 18),
+                                              Text(
+                                                testsListDb.elementAt(index).testName,
+                                                style: Theme.of(context).textTheme.bodyText1,
+                                              ),
+                                            ],
+                                          ),
+                                          subtitle: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 10),
+                                              Text('Datum.: ' +
+                                                  DateFormat('dd.MM.yyyy').format(
+                                                      testsListDb
+                                                          .elementAt(index)
+                                                          .testDate)),
+                                              SizedBox(height: 8),
+                                              Text('Test-ID: ' +
+                                                  testsListDb
+                                                      .elementAt(index)
+                                                      .testId),
+                                              SizedBox(height: 8),
+                                              Text(testsListDb
+                                                      .elementAt(index)
+                                                      .testStatus.toString()),
+                                              SizedBox(height: 18),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+            ),
+          ],
         ),
       ),
       drawer: MyDrawer(),
@@ -148,10 +103,11 @@ class _MyTestPageState extends State<MyTestPage> {
   }
 }
 
-List<Test> vaccinationListDb = List<Test>.generate(20, (int i) {
-  return Test.newlyWithoutStatus(
+List<Test> testsListDb = List<Test>.generate(20, (int i) {
+  return Test(
       faker.lorem.word(),
       faker.randomGenerator.decimal().toString(),
       faker.date.dateTime(),
+      faker.randomGenerator.element(Status.values),
       faker.lorem.sentence());
 });
